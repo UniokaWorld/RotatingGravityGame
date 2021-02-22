89382eefb47f258adfcafbf76502770160757758
UniokaWorld <uniokaworld@gmail.com>
2021-02-22 21:20:05 +0900

    modal

---
+++ Assets/Scripts/Modal.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class RotateModal : MonoBehaviour
+{
+    [SerializeField] private FloatReactiveProperty _rotate = new FloatReactiveProperty();
+
+    public IReadOnlyReactiveProperty<float> Rotate => _rotate;
+}
