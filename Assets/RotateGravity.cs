ce862cb1fbb607cd6c60a85e80092fdbdfd81b8c
UniokaWorld <uniokaworld@gmail.com>
2021-02-22 12:09:37 +0900

    カメラが移動すると適切に動かないのを修正

--- Assets/RotateGravity.cs
+++ Assets/RotateGravity.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class RotateGravity : MonoBehaviour
 {
-    Plane plane = new Plane();
-    float distance = 0;
-
     void Update()
     {
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        var lookPoint = ray.GetPoint(distance);
-        transform.LookAt(transform.localPosition + Vector3.forward, lookPoint);
-        float phase = transform.eulerAngles.z * (Mathf.PI / 180);
+        Vector2 camera_center =
+            new Vector2(Camera.main.transform.position.x, Camera.main.transform.position.y);
+        Vector2 mouse_pointer = new Vector2(ray.origin.x, ray.origin.y);
+        Vector2 dt = mouse_pointer - camera_center;
+        float rad = Mathf.Atan2(dt.y, dt.x);
+        float degree = (rad * Mathf.Rad2Deg) + 270;
+        Debug.Log(degree);
+        float phase = degree * (Mathf.PI / 180);
 
         float xPos = 1.8f * Mathf.Cos(phase);
         float yPos = 1.8f * Mathf.Sin(phase);
