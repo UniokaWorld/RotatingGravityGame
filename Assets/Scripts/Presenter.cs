eca0dbe115ae13d10865f91869b8eeade3af2aae
UniokaWorld <uniokaworld@gmail.com>
2021-02-22 21:20:34 +0900

    Presenter

---
+++ Assets/Scripts/Presenter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class Presenter : MonoBehaviour
+{
+    [SerializeField] private RotateModal _Modal;
+
+    [SerializeField] private RectTransform _Arrow;
+
+    void Start()
+    {
+        _Modal.Rotate.Subscribe(x => { _Arrow.Rotate(new Vector3(0, 0, x)); }).AddTo(this);
+    }
+}
