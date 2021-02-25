675a0223d450ce53e5fab94a7878728ee850517c
UniokaWorld <uniokaworld@gmail.com>
2021-02-25 23:46:34 +0900

    PUI

--- Assets/Scripts/Presenter.cs
+++ Assets/Scripts/Presenter.cs
@@ -9,10 +9,12 @@ public class Presenter : MonoBehaviour
     [SerializeField] private Modal _Modal;
     [SerializeField] private RectTransform _Arrow;
     [SerializeField] private TextMeshProUGUI coinUI;
+    [SerializeField] private GameObject Messege;
 
     void Start()
     {
         _Modal.Rotate.Subscribe(x => { _Arrow.eulerAngles = new Vector3(0, 0, x); }).AddTo(this);
         _Modal.Coin.Subscribe(x => coinUI.text = x).AddTo(this);
+        _Modal.P_Start.Subscribe(x => Messege.SetActive(!x));
     }
 }
