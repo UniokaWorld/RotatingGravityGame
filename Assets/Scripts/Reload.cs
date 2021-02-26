ec14484d3d15724013704d805c0b6bfdac88fc3b
UniokaWorld <uniokaworld@gmail.com>
2021-02-26 23:36:07 +0900

    R長押しで再読み込みするよー

---
+++ Assets/Scripts/Reload.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine.SceneManagement;
+
+public class Reload : MonoBehaviour
+{
+    void Start()
+    {
+        var mouseDownStream = this.UpdateAsObservable().Where(_ => Input.GetKeyDown(KeyCode.R));
+        var mouseUpStream = this.UpdateAsObservable().Where(_ => Input.GetKeyUp(KeyCode.R));
+
+        //長押しの判定
+        mouseDownStream
+            //マウスクリックされたら3秒後にOnNextを流す
+            .SelectMany(_ => Observable.Timer(TimeSpan.FromSeconds(1)))
+            //途中でMouseUpされたらストリームをリセット
+            .TakeUntil(mouseUpStream)
+            .RepeatUntilDestroy(this.gameObject)
+            .Subscribe(_ => SceneManager.LoadScene(SceneManager.GetActiveScene().name));
+    }
+}
